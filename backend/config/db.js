405290a383ccd3f547fd6fc4c7ac1669f77982a7
koyelsudanii <koyelsudanii30@gmail.com>
2025-07-30 17:12:54 +0530

    fsd first

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+//mongodb connection configuration
+
+const mongoose = require('mongoose');
+
+//create the function to connect to the mongodb database
+const connectDB = async () => {
+    try{
+        const connection = await mongoose.Schema()
+
+
+    } catch (error)
+    {
+        console.log("connection error")
+    }
+}
+
+module.exports = connectDB;
